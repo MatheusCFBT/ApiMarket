1e17b5ce970a2424a8882388d5df3695acec044b
MatheusCFBT <matheusterzini@gmail.com>
2024-08-16 11:44:29 -0300

    Add Business Layer and DTO's

---
+++ src/DevApi/ViewModels/FornecedorViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevApi.ViewModels
+{
+    public class FornecedorViewModel
+    {
+        [Key] 
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(70, MinimumLength = 2, ErrorMessage = "O campo {0} precisa conter entre {2} a {1} caracteres")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(14, MinimumLength = 11, ErrorMessage = "O campo {0} precisa conter entre {2} a {1} caracteres")]
+        public string? Documento { get; set; }
+
+        public int TipoFornecedor { get; set; }
+
+        public EnderecoViewModel? Endereco { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public IEnumerable<ProdutoViewModel>? Produtos { get; set; }
+    }
+}
