55776c2256f12a6d0eea83a8620331d46b2756d0
MatheusCFBT <matheusterzini@gmail.com>
2024-08-19 18:04:14 -0300

    Setup Layers and (on configuring) Mapping Entitys on Db

---
+++ src/DevData/Mappings/FornecedorMapping.cs
@@ -0,0 +1,37 @@
+﻿using DevBusiness.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevData.Mappings
+{
+    public class FornecedorMapping : IEntityTypeConfiguration<Fornecedor>
+    {
+        public void Configure(EntityTypeBuilder<Fornecedor> builder)
+        {
+            builder.HasKey(f=> f.Id);
+
+            builder.Property(f => f.Nome)
+                .IsRequired()
+               .HasColumnType("varchar(200)");
+
+            builder.Property(f => f.Documento)
+                .IsRequired()
+               .HasColumnType("varchar(14)");
+
+            builder.HasOne(f => f.Endereco)
+                .WithOne(e => e.Fornecedor);
+
+            builder.HasMany(f => f.Produtos)
+                .WithOne(p => p.Fornecedor)
+                .HasForeignKey(p => p.FornecedorId);
+
+
+            builder.ToTable("Fornecedores");
+        }
+    }
+}
