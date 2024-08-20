18c7a3c4cd7701192414803701492389c1fe634c
MatheusCFBT <matheusterzini@gmail.com>
2024-08-20 13:34:19 -0300

    Add Pattern Repository

---
+++ src/DevBusiness/Interfaces/IEnderecoRepository.cs
@@ -0,0 +1,14 @@
+﻿using DevBusiness.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBusiness.Interfaces
+{
+    public interface IEnderecoRepository : IRepository<Endereco>    
+    {
+        Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId);
+    }
+}
---
+++ src/DevData/Repository/ProdutoRepository.cs
@@ -0,0 +1,33 @@
+﻿using DevBusiness.Interfaces;
+using DevBusiness.Models;
+using DevData.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevData.Repository
+{
+    public class ProdutoRepository : Repository<Produto>, IProdutoRepository
+    {
+        public ProdutoRepository(MeuDbContext context) : base(context) { }
+        public async Task<Produto> ObterProdutoFornecedor(Guid id)
+        {
+            return await meuDb.Produtos.AsNoTracking().Include(f => f.Fornecedor)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Produto>> ObterProdutoPorFornecedor(Guid fornecedorId)
+        {
+            return await Buscar(p => p.FornecedorId == fornecedorId);
+        }
+
+        public async Task<IEnumerable<Produto>> ObterProdutosFornecedores()
+        {
+            return await meuDb.Produtos.AsNoTracking().Include(f => f.Fornecedor)
+                .OrderBy(p => p.Nome).ToListAsync();
+        }
+    }
+}
