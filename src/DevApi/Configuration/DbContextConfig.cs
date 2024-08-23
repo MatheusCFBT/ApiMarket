393e6c3437e423b03306d16e22817319ba0ee075
MatheusCFBT <matheusterzini@gmail.com>
2024-08-23 11:31:06 -0300

    Clean Program encapsulating configs

---
+++ src/DevApi/Configuration/DbContextConfig.cs
@@ -0,0 +1,17 @@
+﻿using DevData.Context;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevApi.Configuration
+{
+    public static class DbContextConfig
+    {
+        public static WebApplicationBuilder AddDbContextConfig(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<MeuDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            return builder;
+        }
+    }
+}
--- src/DevApi/Program.cs
+++ src/DevApi/Program.cs
@@ -1,3 +1,4 @@
+using DevApi.Configuration;
 using DevBusiness.Interfaces;
 using DevBusiness.Notificacoes;
 using DevBusiness.Services;
@@ -7,25 +8,13 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-
 builder.Services.AddAutoMapper(typeof(Program));
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<MeuDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddScoped<MeuDbContext>();
-builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
-builder.Services.AddScoped<IFornecedorRepository, FornecedorRepository>();
-builder.Services.AddScoped<IEnderecoRepository, EnderecoRepository>();
-builder.Services.AddScoped<INotificador, Notificador>();
-builder.Services.AddScoped<IFornecedorService, FornecedorService>();
-builder.Services.AddScoped<IProdutoService, ProdutoService>();
+builder
+    .AddApiConfig()
+    .AddSwaggerConfig()
+    .ResolveDependencies()
+    .AddDbContextConfig();
 
 var app = builder.Build();
 
